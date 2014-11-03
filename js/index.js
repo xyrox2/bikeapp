8c09bd8f9e5bc91ccd3331243b9cc3cca34b7136
Roman Lewin <rome.gregori@googlemail.com>
2014-11-03 14:51:45 +0100

    Map included

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ var app = {
     init: function() {
         $(document).ready(function() {
           // Handler for .ready() called.
-          document.addEventListener("deviceready", onDeviceReady, true);
+          document.addEventListener("deviceready", this.onDeviceReady, true);
         });
+
+        this.maps();
     },
     // deviceready Event Handler
     //
@@ -29,5 +31,72 @@ var app = {
     },
     login: function() {
 
+    },
+    maps: function() {
+        var map;
+
+        function initialize() {
+          var mapOptions = {
+            zoom: 6
+          };
+          map = new google.maps.Map(document.getElementById('map-canvas'),
+              mapOptions);
+
+          // Try HTML5 geolocation
+          if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = new google.maps.LatLng(position.coords.latitude,
+                                               position.coords.longitude);
+
+              var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Location found using HTML5.'
+              });
+
+              map.setCenter(pos);
+            }, function() {
+              handleNoGeolocation(true);
+            });
+          } else {
+            // Browser doesn't support Geolocation
+            handleNoGeolocation(false);
+          }
+        }
+
+        function handleNoGeolocation(errorFlag) {
+          if (errorFlag) {
+            var content = 'Error: The Geolocation service failed.';
+          } else {
+            var content = 'Error: Your browser doesn\'t support geolocation.';
+          }
+
+          var options = {
+            map: map,
+            position: new google.maps.LatLng(60, 105),
+            content: content
+          };
+
+          var infowindow = new google.maps.InfoWindow(options);
+          map.setCenter(options.position);
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+        function getRealContentHeight() {
+            var header = $.find("div[data-role='header']:visible");
+            var footer = $.find("div[data-role='footer']:visible");
+            var content = $.find("div[data-role='content']:visible:visible");
+            var viewport_height = $(window).height();
+
+            var content_height = viewport_height - $(header).outerHeight() - $(footer).outerHeight();
+            if(($(content).outerHeight() - $(header).outerHeight() - $(footer).outerHeight()) <= viewport_height) {
+                content_height -= ($(content).outerHeight() - $(content).height());
+            }
+            console.log(content_height);
+            return content_height;
+        }
+
+        $('#map-canvas').height(getRealContentHeight());
     }
 };
